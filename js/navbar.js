4d2a8f19def0de6c98edbfa4bbab002816094828
© MLabs <hpmewes@gmx.de>
2014-11-11 01:52:27 +0100

    dropdown now closed on second click
    better user experience on mobile devices

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
     $('.navbar a.dropdown-toggle').on('click', function(e) {
-        if (!$(this).parent().parent().hasClass('nav')) {
-            var heightParent = parseInt($(this).parent().parent().css('height').replace('px', '')) / 2;
-            var widthParent = parseInt($(this).parent().parent().css('width').replace('px', '')) - 10;
-            $(this).parent().addClass('open');
+        var elmnt = $(this).parent().parent();
+        if (!elmnt.hasClass('nav')) {
+            var li = $(this).parent();
+            var heightParent = parseInt(elmnt.css('height').replace('px', '')) / 2;
+            var widthParent = parseInt(elmnt.css('width').replace('px', '')) - 10;
+            
+            if(!li.hasClass('open')) li.addClass('open')
+            else li.removeClass('open');
             $(this).next().css('top', heightParent + 'px');
             $(this).next().css('left', widthParent + 'px');
+            
             return false;
         }
     });
