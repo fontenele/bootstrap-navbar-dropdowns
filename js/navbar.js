9d1cfe8072af8b8d7925d5c4e7a6b8ef8aadd997
Guilherme Fontenele <guilherme@fontenele.net>
2016-11-14 23:32:14 -0200

    Fix example

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     $('.navbar a.dropdown-toggle').on('click', function(e) {
-
-        var elmnt = $(this).offsetParent(".dropdown-menu");
-        var heightParent = (elmnt.outerHeight() / 2);
-        var widthParent = (elmnt.outerWidth() - 10);
-
-        $(this).next().css({ "top": heightParent, "left": widthParent });
+        var $el = $(this);
+        var $parent = $(this).offsetParent(".dropdown-menu");
         $(this).parent("li").toggleClass('open');
 
-        $('.navbar li.open').not($(this).parents("li")).removeClass("open");
+        if(!$parent.parent().hasClass('nav')) {
+            $el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
+        }
+
+        $('.nav li.open').not($(this).parents("li")).removeClass("open");
 
         return false;
     });
