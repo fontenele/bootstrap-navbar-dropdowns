67d1302f391d5421e656e64676d6b239a1aefdae
Guilherme Fontenele <guilherme@fontenele.net>
2018-02-16 19:37:10 -0200

    https://github.com/fontenele/bootstrap-navbar-dropdowns/issues/11 submenu open in the left side of parent

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,18 @@
-$(document).ready(function() {
-    $('.navbar a.dropdown-toggle').on('click', function(e) {
+$(document).ready(function () {
+    $('.navbar a.dropdown-toggle').on('click', function (e) {
         var $el = $(this);
         var $parent = $(this).offsetParent(".dropdown-menu");
         $(this).parent("li").toggleClass('open');
 
-        if(!$parent.parent().hasClass('nav')) {
-            $el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
+        console.log(45, $parent.closest('.nav'));
+        if (!$parent.parent().hasClass('nav')) {
+            if ($parent.closest('.nav').hasClass('navbar-right')) {
+                console.log('aaa');
+                $el.next().css({"top": $el[0].offsetTop, "right": $parent.outerWidth() - 4});
+            } else {
+                console.log('bbb');
+                $el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
+            }
         }
 
         $('.nav li.open').not($(this).parents("li")).removeClass("open");
