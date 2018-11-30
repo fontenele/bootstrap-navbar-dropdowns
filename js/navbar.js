6f974b8188bc3e23644404b2799ed93c526d5df5
Guilherme Fontenele <guilherme@fontenele.net>
2018-11-30 16:07:18 -0200

    hotfix - open link when it is just a link

--- js/navbar.js
+++ js/navbar.js
@@ -1,23 +1,26 @@
 $(document).ready(function () {
 
-    $('.navbar .dropdown-item').on('click', function (e) {
+    $('.navbar .dropdown-item.dropdown').on('click', function (e) {
         var $el = $(this).children('.dropdown-toggle');
-        var $parent = $el.offsetParent(".dropdown-menu");
-        $(this).parent("li").toggleClass('open');
-
-        if (!$parent.parent().hasClass('navbar-nav')) {
-            if ($parent.hasClass('show')) {
-                $parent.removeClass('show');
-                $el.next().removeClass('show');
-                $el.next().css({"top": -999, "left": -999});
-            } else {
-                $parent.parent().find('.show').removeClass('show');
-                $parent.addClass('show');
-                $el.next().addClass('show');
-                $el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
+        if ($el.length > 0 && $(e.target).hasClass('dropdown-toggle')) {
+            var $parent = $el.offsetParent(".dropdown-menu");
+            $(this).parent("li").toggleClass('open');
+    
+            if (!$parent.parent().hasClass('navbar-nav')) {
+                if ($parent.hasClass('show')) {
+                    $parent.removeClass('show');
+                    $el.next().removeClass('show');
+                    $el.next().css({"top": -999, "left": -999});
+                } else {
+                    $parent.parent().find('.show').removeClass('show');
+                    $parent.addClass('show');
+                    $el.next().addClass('show');
+                    $el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
+                }
+                e.preventDefault();
+                e.stopPropagation();
             }
-            e.preventDefault();
-            e.stopPropagation();
+            return;
         }
     });
 
