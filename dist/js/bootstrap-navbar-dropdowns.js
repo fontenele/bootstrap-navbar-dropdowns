32576c8616fb3d8e2d0ba94317af9e6a98ff33ef
Guilherme Fontenele <guilherme@fontenele.net>
2020-10-18 15:51:08 -0300

    #19 transform into jquery plugin

--- dist/js/bootstrap-navbar-dropdowns.js
+++ dist/js/bootstrap-navbar-dropdowns.js
@@ -1,38 +1,75 @@
-$(document).ready(function () {
-  $('.navbar .dropdown-item.dropdown').on('click', function (e) {
-    var $el = $(this).children('.dropdown-toggle');
+(function ($) {
+  $.fn.navbarDropdown = function () {
+    $(this).find('.dropdown-item.dropdown').on('click', function (e) {
+      var $el = $(this).children('.dropdown-toggle');
 
-    if ($el.length > 0 && $(e.target).hasClass('dropdown-toggle')) {
-      var $parent = $el.offsetParent(".dropdown-menu");
-      $(this).parent("li").toggleClass('open');
+      if ($el.length > 0 && $(e.target).hasClass('dropdown-toggle')) {
+        var $parent = $el.offsetParent(".dropdown-menu");
+        $(this).parent("li").toggleClass('open');
 
-      if (!$parent.parent().hasClass('navbar-nav')) {
-        if ($parent.hasClass('show')) {
-          $parent.removeClass('show');
-          $el.next().removeClass('show');
-          $el.next().css({
-            "top": -999,
-            "left": -999
-          });
-        } else {
-          $parent.parent().find('.show').removeClass('show');
-          $parent.addClass('show');
-          $el.next().addClass('show');
-          $el.next().css({
-            "top": $el[0].offsetTop,
-            "left": $parent.outerWidth() - 4
-          });
+        if (!$parent.parent().hasClass('navbar-nav')) {
+          if ($parent.hasClass('show')) {
+            $parent.removeClass('show');
+            $el.next().removeClass('show');
+            $el.next().css({
+              "top": -999,
+              "left": -999
+            });
+          } else {
+            $parent.parent().find('.show').removeClass('show');
+            $parent.addClass('show');
+            $el.next().addClass('show');
+            $el.next().css({
+              "top": $el[0].offsetTop,
+              "left": $parent.outerWidth() - 4
+            });
+          }
+
+          e.preventDefault();
+          e.stopPropagation();
         }
 
-        e.preventDefault();
-        e.stopPropagation();
+        return this;
       }
+    });
+    $(this).find('.dropdown').on('hidden.bs.dropdown', function () {
+      $(this).find('li.dropdown').removeClass('show open');
+      $(this).find('ul.dropdown-menu').removeClass('show open');
+    });
+    return this;
+  };
+})(jQuery);
+/*
+$(document).ready(function () {
 
-      return;
-    }
-  });
-  $('.navbar .dropdown').on('hidden.bs.dropdown', function () {
-    $(this).find('li.dropdown').removeClass('show open');
-    $(this).find('ul.dropdown-menu').removeClass('show open');
-  });
+    $('.navbar .dropdown-item.dropdown').on('click', function (e) {
+        var $el = $(this).children('.dropdown-toggle');
+        if ($el.length > 0 && $(e.target).hasClass('dropdown-toggle')) {
+            var $parent = $el.offsetParent(".dropdown-menu");
+            $(this).parent("li").toggleClass('open');
+
+            if (!$parent.parent().hasClass('navbar-nav')) {
+                if ($parent.hasClass('show')) {
+                    $parent.removeClass('show');
+                    $el.next().removeClass('show');
+                    $el.next().css({"top": -999, "left": -999});
+                } else {
+                    $parent.parent().find('.show').removeClass('show');
+                    $parent.addClass('show');
+                    $el.next().addClass('show');
+                    $el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
+                }
+                e.preventDefault();
+                e.stopPropagation();
+            }
+            return;
+        }
+    });
+
+    $('.navbar .dropdown').on('hidden.bs.dropdown', function () {
+        $(this).find('li.dropdown').removeClass('show open');
+        $(this).find('ul.dropdown-menu').removeClass('show open');
+    });
+
 });
+*/
